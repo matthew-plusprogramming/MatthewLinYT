965f77946cc04a9fc58eda66d90b877cd46f6f30
matthew-plusprogramming <matthewlinplusprogramming@gmail.com>
2020-11-27 21:00:28 -0800

    :page_facing_up: Added ep 6 code

---
+++ JSForBeginners/Ep6-Arrays/index.js
@@ -0,0 +1,10 @@
+// Array, printing, pushing, removing, looping
+
+let emailList = [];
+
+emailList.push('example@example.com');
+emailList.push('example@example2.com');
+
+for (let i = 0; i < emailList.length; i++) {
+  console.log(emailList[i]);
+}
