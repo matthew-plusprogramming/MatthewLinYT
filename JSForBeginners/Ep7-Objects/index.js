e781cc939069e7751fd635bdf935ba4dcd0b875f
matthew-plusprogramming <matthewlinplusprogramming@gmail.com>
2020-11-28 22:11:32 -0800

    :page_facing_up: Added ep 7 code

---
+++ JSForBeginners/Ep7-Objects/index.js
@@ -0,0 +1,14 @@
+let peopleDictionary = {
+  matthew: {
+    name: 'Matthew',
+    age: 16,
+    favoriteColor: 'blue',
+  },
+  james: {
+    name: 'James',
+    age: 14,
+    favoriteColor: 'grey',
+  },
+};
+
+console.log(peopleDictionary.matthew.age);
