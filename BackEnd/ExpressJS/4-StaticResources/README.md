6b058b501b26300b3011b1a6f50693df0f53c33a
matthew-plusprogramming <matthewlinplusprogramming@gmail.com>
2021-02-28 15:00:00 -0800

    📄 Add #4 - Static Resources

---
+++ BackEnd/ExpressJS/4-StaticResources/README.md
@@ -0,0 +1,8 @@
+# 🎞️ Episode 4 - Static Resources
+
+## 📝 In this video:
+- [x] File hosting servers (explanation)
+- [x] Express static
+
+## 🔗 Resources
+
---
+++ BackEnd/ExpressJS/4-StaticResources/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+const data = {
+  title: 'Atomic Habits',
+  author: 'James Clear',
+};
+
+app.use(express.static('static'));
+
+app.get('/hello', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+app.get('/', (req, res) => {
+  res.send(JSON.stringify(data));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
